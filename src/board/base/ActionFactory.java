27390975a1e170c0f58f267b2d5bab4273f25f18
choi <choi.dahye424@gmail.com>
2021-11-01 21:03:07 +0900

    자바 팀프로젝트 언택트투어v0.1

---
+++ src/board/base/ActionFactory.java
@@ -0,0 +1,21 @@
+package board.base;
+
+public class ActionFactory {
+
+	public Action getAction(String command) {
+		
+		Action action = null;
+		
+//		switch(command) {
+//		// MYPAGE
+//		case "": break;
+//		
+//		// CATEGORY
+//		case "": break;
+//		
+//		// DETAIL
+//		case "": break;		
+//	}	
+		return action;
+	}
+}
